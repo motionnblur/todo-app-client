7675987669a6ba674d6f8415d099628fc11d7b35
chill <chill@mail.com>
2024-08-16 14:25:11 +0300

    Open left menu

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,11 +12,17 @@ export default function page() {
   const MainCanvas = () => {
     return <div className="h-full w-full bg-neutral-800"></div>;
   };
+  const openLeftMenu = () => {
+    setLeftMenu(!leftMenu);
+  };
   return (
     <main className="w-screen h-screen flex flex-col">
       <div className="w-full h-[8vh] bg-zinc-900 flex flex-row">
         <div className="h-full w-full p-1 flex items-center gap-4">
-          <div className="h-10 w-10 bg-black rounded-full ml-4 cursor-pointer flex items-center justify-center">
+          <div
+            className="h-10 w-10 bg-black rounded-full ml-4 cursor-pointer flex items-center justify-center"
+            onMouseUp={openLeftMenu}
+          >
             <AlignJustify className="rotate-90" />
           </div>
           <div className="w-24 h-4/5 bg-black rounded-md flex justify-center items-center cursor-pointer ml-4">
