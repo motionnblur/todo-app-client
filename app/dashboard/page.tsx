330db80308b83ed8c1437a536a2b70ad6e2ffe15
chill <chill@mail.com>
2024-08-16 14:21:51 +0300

    ..

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { AlignJustify } from "react-feather";
 
 export default function page() {
-  const [leftMenu, setLeftMenu] = useState<Boolean>();
+  const [leftMenu, setLeftMenu] = useState<Boolean>(false);
 
   const LeftMenu = () => {
     return <div className="h-full w-[28vh] bg-zinc-950"></div>;
@@ -17,7 +17,7 @@ export default function page() {
       <div className="w-full h-[8vh] bg-zinc-900 flex flex-row">
         <div className="h-full w-full p-1 flex items-center gap-4">
           <div className="h-10 w-10 bg-black rounded-full ml-4 cursor-pointer flex items-center justify-center">
-            <AlignJustify />
+            <AlignJustify className="rotate-90" />
           </div>
           <div className="w-24 h-4/5 bg-black rounded-md flex justify-center items-center cursor-pointer ml-4">
             Dashboard
@@ -29,7 +29,7 @@ export default function page() {
         <div className="h-full w-1/6 p-1 flex items-center"></div>
       </div>
       <div className="w-full h-full flex flex-row">
-        <LeftMenu />
+        {leftMenu && <LeftMenu />}
         <MainCanvas />
       </div>
     </main>
