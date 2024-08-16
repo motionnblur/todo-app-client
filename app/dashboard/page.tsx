effc60c4b69ed7957d3b1d3d6b46228945270993
chill <chill@mail.com>
2024-08-16 14:11:27 +0300

    .

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-import { User } from "react-feather";
+"use client";
+
+import React, { useState } from "react";
+import { AlignJustify } from "react-feather";
 
 export default function page() {
+  const [leftMenu, setLeftMenu] = useState<Boolean>();
+
   const LeftMenu = () => {
     return <div className="h-full w-[28vh] bg-zinc-950"></div>;
   };
@@ -13,7 +17,7 @@ export default function page() {
       <div className="w-full h-[8vh] bg-zinc-900 flex flex-row">
         <div className="h-full w-full p-1 flex items-center gap-4">
           <div className="h-10 w-10 bg-black rounded-full ml-4 cursor-pointer flex items-center justify-center">
-            <User />
+            <AlignJustify />
           </div>
           <div className="w-24 h-4/5 bg-black rounded-md flex justify-center items-center cursor-pointer ml-4">
             Dashboard
