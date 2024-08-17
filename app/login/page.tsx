88429224a15b0e6281c5f15d4410ef497ae5fe35
chill <chill@mail.com>
2024-08-17 06:47:55 +0300

    Add login ui template

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <main className="w-screen h-screen">
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="w-52 h-80 bg-slate-300 mb-14"></div>
+      </div>
+    </main>
+  );
+}
