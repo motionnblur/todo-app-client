d605fa06cc9418302085f6516a903bbe185142a3
chill <chill@mail.com>
2024-08-17 06:55:52 +0300

    .

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,9 @@ export default function page() {
     <main className="w-screen h-screen">
       <div className="w-full h-full flex justify-center items-center">
         <div className="w-52 h-80 bg-slate-300 rounded-xl mb-14 flex flex-col p-2">
-          <div className="w-full h-12"></div>
+          <div className="w-full h-12 flex items-center justify-center text-black text-2xl">
+            Login
+          </div>
           <div className="w-full h-full"></div>
           <div className="w-full h-24"></div>
         </div>
