db17535fb6bcf62ee126331e886414dc3d9c8f9c
chill <chill@mail.com>
2024-08-17 07:01:11 +0300

    ...

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,12 +4,22 @@ export default function page() {
   return (
     <main className="w-screen h-screen">
       <div className="w-full h-full flex justify-center items-center">
-        <div className="w-52 h-80 bg-slate-300 rounded-xl mb-14 flex flex-col p-3">
-          <div className="w-full h-12 flex items-center justify-center text-black text-2xl">
+        <div className="w-52 h-80 bg-slate-300 rounded-xl mb-14 flex flex-col p-5">
+          <div className="w-full h-12 flex items-center justify-center text-black text-xl">
             Login
           </div>
-          <div className="w-full h-full flex flex-col items-center justify-center">
-            <input></input>
+          <div className="w-full h-full flex flex-col items-center justify-center gap-3">
+            <input
+              type="text"
+              className="w-full h-10 rounded-md text-stone-900"
+              placeholder="  Username"
+            />
+            <input
+              type="text"
+              className="w-full h-10 rounded-md text-stone-900"
+              placeholder="  Password"
+            />
+            <div className="w-full h-4"></div>
           </div>
           <div className="w-full h-24"></div>
         </div>
