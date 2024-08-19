916e90bf54269547664f4e6e101fc6340c6ec793
chill <chill@mail.com>
2024-08-19 03:01:40 +0300

    Add function to show password

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 export default function page() {
+  const [typePassword, setTypePassword] = useState<String>("password");
+
+  const getPasswState = (): string => {
+    if (typePassword === "password") return "text";
+    return "password";
+  };
+
   return (
     <main className="w-screen h-screen">
       <div className="w-full h-full flex justify-center items-center">
@@ -12,30 +20,33 @@ export default function page() {
             <input
               type="text"
               className="w-full h-10 rounded-md text-stone-900 p-2"
-              placeholder="  Username"
+              placeholder=" Username"
             />
             <input
-              type="password"
+              type={`${typePassword}`}
               className="w-full h-10 rounded-md text-stone-900 p-2"
-              placeholder="  Password"
+              placeholder=" Password"
             />
             <div className="w-full h-6 flex flex-row">
               <input
                 type="checkbox"
                 className="w-1/6 h-4 rounded-md cursor-pointer"
+                onMouseUp={() => {
+                  setTypePassword(getPasswState);
+                }}
               />
-              <div className="text-black text-xs ml-1">Show Password</div>
+              <div className="text-black text-xs ml-1">Show password</div>
             </div>
           </div>
           <div className="w-full h-32 flex justify-center items-center flex-col gap-1">
-            <div className="w-full h-8 bg-cyan-500 rounded-xl flex items-center justify-center text-sl cursor-pointer">
+            <div className="w-full h-9 bg-cyan-500 rounded-xl flex items-center justify-center text-sl cursor-pointer">
               Login
             </div>
             <div className="text-black text-xs flex flex-row gap-1">
-              Forgot{" "}
+              Forgot
               <div className="text-sky-500 text-xs font-bold cursor-pointer">
                 password
-              </div>{" "}
+              </div>
               ?
             </div>
             <div className="text-sky-500 text-xs font-bold cursor-pointer">
