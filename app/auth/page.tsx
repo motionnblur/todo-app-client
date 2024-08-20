91ee2ebbf3706f29940246e6903e1478e9b55159
chill <chill@mail.com>
2024-08-20 13:26:48 +0300

    auth

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,13 +2,17 @@
 
 import React, { useEffect } from "react";
 import { auth } from "./auth";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function page() {
+  const { push } = useRouter();
+
   useEffect(() => {
     auth()
       .then((isAuthenticated) => {
-        console.log(isAuthenticated);
+        if (!isAuthenticated) {
+          push("/login");
+        }
       })
       .catch((error) => {
         console.error("Authentication error:", error);
