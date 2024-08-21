6ebeecc0f234289579aa3403794ea14e58a8651e
chill <chill@mail.com>
2024-08-21 20:00:07 +0300

    Jotai works

--- components/LeftMenu.tsx
+++ components/LeftMenu.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { useSetAtom } from "jotai";
+import { createTodoAtom } from "@/state/manager";
 
 export default function LeftMenu() {
+  const use_setTodoAtom = useSetAtom(createTodoAtom);
+
   return (
     <div className="h-full w-[28vh] bg-zinc-950 flex flex-col z-10">
       <div className="w-full h-full p-2 flex flex-col">
-        <div className="w-full h-8 bg-slate-800 rounded-md mt-2 cursor-pointer flex justify-center items-center">
+        <div
+          className="w-full h-8 bg-slate-800 rounded-md mt-2 cursor-pointer flex justify-center items-center"
+          onMouseUp={() => {
+            use_setTodoAtom(true);
+          }}
+        >
           Create todo
         </div>
         <div className="w-full h-8 bg-slate-800 rounded-md mt-2 cursor-pointer flex justify-center items-center">
--- components/MainCanvas.tsx
+++ components/MainCanvas.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import MyTodoList from "./MyTodoList";
+import { useAtomValue } from "jotai";
+import { createTodoAtom } from "@/state/manager";
 
 export default function MainCanvas() {
+  const use_setTodoAtom = useAtomValue(createTodoAtom);
+  console.log("a");
+
   return (
     <div className="w-full h-full flex justify-center flex-wrap gap-3 p-2">
       <MyTodoList />
