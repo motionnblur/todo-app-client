883d515d2f4f428cc55da6c09bf36520b7fc2370
chill <chill@mail.com>
2024-08-23 16:15:48 +0300

    Fix
    fix LeftMenu

--- components/LeftMenu.tsx
+++ components/LeftMenu.tsx
@@ -7,7 +7,7 @@ export default function LeftMenu() {
   const [todoListAtom, setTodoListAtom] = useAtom(myTodoListAtom);
 
   return (
-    <div className="h-full w-[28vh] bg-zinc-950 flex flex-col z-10">
+    <div className="h-full w-[28vh] bg-zinc-950 flex flex-col z-10 fixed">
       <div className="w-full h-full p-2 flex flex-col">
         <div
           className="w-full h-8 bg-slate-800 rounded-md mt-2 cursor-pointer flex justify-center items-center"
